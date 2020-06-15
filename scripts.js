b176b856b8cded28eb6bb5c475642afb47cc1b0f
medhat <modeht@gmail.com>
2020-06-15 16:59:12 +0200

    created to modals for all buttons

--- scripts.js
+++ scripts.js
@@ -1,1 +1,47 @@
-console.log("hello");
+let counter=0;
+function hideOtherButtons(obj){
+    counter++;
+    console.log(counter);
+    obj.parentNode.childNodes.forEach(node => {
+        if (node.classList[0] == obj.classList[0]){
+            return true;            
+        }
+        else{
+            node.setAttribute('hidden','hidden');
+        }                        
+    });
+}
+
+
+document.querySelector('.profile').addEventListener('click',function (){
+    do_work(this);
+});
+
+document.querySelector('.github').addEventListener('click',function (){
+    do_work(this);
+});
+
+document.querySelector('.phone').addEventListener('click',function (){
+    do_work(this);
+});
+
+document.querySelector('.mail').addEventListener('click',function (){
+    do_work(this);
+});
+
+document.querySelector('.address').addEventListener('click',function (){
+    do_work(this);
+});
+
+function do_work(obj) {
+    hideOtherButtons(obj);
+    obj.classList.add('expand');
+    obj.childNodes.forEach(node => {
+        if(node.classList == 'bar'){
+            node.lastElementChild.removeAttribute('hidden');
+        };
+        if(node.classList == 'body'){
+            node.removeAttribute('hidden');
+        }
+    })
+}
