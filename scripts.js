4af3cfbfdc560f68eafd96bfe428cf7bb8e7a613
medhat <modeht@gmail.com>
2020-06-19 11:29:32 +0200

    finished x functionality and card

--- scripts.js
+++ scripts.js
@@ -1,4 +1,72 @@
+let exit_btn = document.querySelectorAll('.exit');
+let card_btn = document.querySelectorAll('.button');
 
+exit_button(exit_btn);
+modal_btn(card_btn);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function exit_button(obj){
+    //logic is i get exit button handler remove open class
+    // from div.bar (that is the class telling me the modal is open)
+    //then i get the grand parent div and remove expand class to remove modal
+    // and add hidden attribute to the things inside again
+    for(var i = 0; i< obj.length;i++){
+        obj[i].addEventListener('click',function (e){
+            e.stopPropagation();
+            this.parentNode.classList.remove('open');
+            let parent_div = this.parentNode.parentNode;
+            parent_div.classList.remove('expand');
+            let grand_parent = parent_div.parentNode;
+            grand_parent.childNodes.forEach(node => {
+                node.removeAttribute('hidden');               
+            });
+            this.setAttribute('hidden','hidden');
+            parent_div.childNodes.forEach(node => {
+                if(node.classList =='body')
+                node.setAttribute('hidden','hidden');
+            });
+        });
+    }
+}
+function modal_btn(obj){
+    for(var i = 0; i < obj.length; i++){
+        obj[i].addEventListener('click',function (e){
+            // e.stopPropagation();
+            open_modal(this);
+        });
+    };
+}
 function hideOtherButtons(obj){    
     obj.parentNode.childNodes.forEach(node => {
         if (node.classList[0] == obj.classList[0]){
@@ -9,11 +77,12 @@ function hideOtherButtons(obj){
         }                        
     });
 }
-function do_work(obj) {
+function open_modal(obj) {
     hideOtherButtons(obj);
     obj.classList.add('expand');
     obj.childNodes.forEach(node => {
         if(node.classList == 'bar'){
+            node.classList.add('open');
             node.lastElementChild.removeAttribute('hidden');
         };
         if(node.classList == 'body'){
@@ -21,24 +90,4 @@ function do_work(obj) {
         }
     })
 }
-
-document.querySelector('.profile').addEventListener('click',function (){
-    do_work(this);
-});
-
-document.querySelector('.github').addEventListener('click',function (){
-    do_work(this);
-});
-
-document.querySelector('.phone').addEventListener('click',function (){
-    do_work(this);
-});
-
-document.querySelector('.mail').addEventListener('click',function (){
-    do_work(this);
-});
-
-document.querySelector('.address').addEventListener('click',function (){
-    do_work(this);
-});
 
